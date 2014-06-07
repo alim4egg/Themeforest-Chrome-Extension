12b6c5fe884ea83db8965b2b60684ed9985e06c2
Michael <michaelgrinstead@Macintosh.local>
2014-06-07 11:22:09 +0100

    Minified css & js

--- chrome_extension/themeforest/tf_script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-	//var percentage = $('.progressbar-value').html();
-	
-	$( "[role=progressbar]" ).wrap( "<div class='progress-wrap clearfix'></div>" );
-	
-	$(".progressbar-value").contents().appendTo('.progress-wrap').wrap( "<span class='percentage'></span>" );
-	
-});
